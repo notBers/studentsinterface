385929a603b5f930948a460a7dca2a170cc10594
notBers <109546794+notBers@users.noreply.github.com>
2024-01-02 19:55:35 -0600

    first sbmit

---
+++ src/components/subcomponents/Login.js
@@ -0,0 +1,28 @@
+import { Link, redirect } from "react-router-dom";
+import { useState } from "react";
+import "../../stylesheets/Login.css";
+
+function Login() {
+  const [identifier, setIdentifier] = useState();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login">
+      <div className="login-container">
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Enter identifier here"
+            onChange={(e) => setIdentifier(e.target.value)}
+          />
+          <Link className="submitlogin" to={identifier}>Submit</Link>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
